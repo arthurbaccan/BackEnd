57f5c291456d15f24dbeb63bea6283f516630fc9
Lucasntunes <168087730+Lucasntunes@users.noreply.github.com>
2025-08-04 09:52:28 -0300

    Add files via upload

---
+++ BackENd/src/test/java/TCC/BackENd/BackENdApplicationTests.java
@@ -0,0 +1,13 @@
+package TCC.BackENd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackENdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
